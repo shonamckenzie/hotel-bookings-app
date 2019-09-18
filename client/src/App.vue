3dde609530bdc471daeda50f6858fd4081d0246c
Suzanne Dyson <suzanne.dyson2334@gmail.com>
2019-09-18 12:15:50 +0100

    vue front-end initial working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  
+  <div id="app">
+  <bookings-list :bookings="bookings" />
+  </div>
 </template>
 
 <script>
-export default {
+import BookingsList from './components/BookingsList';
+import BookingService from './services/BookingService';
 
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      bookings: []
+    }
+  },
+  components: {
+    'bookings-list': BookingsList
+  },
+  mounted(){
+    this.fetchData();
+  },
+  methods: {
+    fetchData(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings);
+    }
+  }
 }
 </script>
 
---
+++ client/src/components/BookingsList.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+  <div id="bookingsList">
+    <div class="booking" v-for="booking in bookings">
+      <h2>{{ booking.name }}</h2>
+      <p>{{ booking.email }}</p>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import BookingService from '../services/BookingService';
+
+export default {
+  name: "bookings-list",
+  props: ["bookings"]
+}
+
+</script>
+
+<style lang="css" scoped>
+</style>
